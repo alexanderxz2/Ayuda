cad893a03beccbc0f5775c3de91c8f44ee0f7018
alexanderxz2 <51862163+alexanderxz2@users.noreply.github.com>
2023-11-14 19:46:37 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -119,7 +119,7 @@
                         'rgba(255, 159, 64, 1)'
                     ],
                     borderWidth: 1,
-                    datalabels: false // Desactiva datalabels para este dataset
+                    datalabels: true // Desactiva datalabels para este dataset
                 }, {
                     type: 'line',
                     data: [fila1, fila2, fila3, fila4, fila5, fila6],
